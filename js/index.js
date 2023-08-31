d4fe22d2c64d3404edf6ab4beea7587818f1c6a0
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-01 01:17:14 +0600

    blog html files finish with responsive and tab daisyui added with js code start

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-const handleVideoCategory = async() =>{
-const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
-const data = await res.json();
+// const handleVideoCategory = async() =>{
+// const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+// const data = await res.json();
 
+// }
+
+let  a = 29; 
+console.log('the value of a is', a); 
+function sum(){
+    let b = 10;
+    let a = 19;
+    console.log('the value of b is', b)
+    console.log('the value of a is', a); 
 }
+sum();
