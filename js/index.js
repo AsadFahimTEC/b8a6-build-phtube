71091cf91f94a472d62d0a6b150c3dc51e8be723
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-01 08:11:53 +0600

    all card show handleCard Section started

--- js/index.js
+++ js/index.js
@@ -30,13 +30,13 @@ const loadVideo = async (categoryId) => {
     div.innerHTML = `
         <div class="card w-96 bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
-    <img src=""${video.thumbnail}" alt="Shoes" class="rounded-xl" />
+    <img src=""${video.video.thumbnail}" alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="${video.title}">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
+      <button onclick= "handleModal()" class="btn btn-primary">Buy Now</button>
     </div>
   </div>
 </div>
@@ -45,5 +45,13 @@ const loadVideo = async (categoryId) => {
   });
 };
 
+// handle card
+
+const handleCard = (categoryId) =>{
+    console.log('clicked',categoryId );
+}
+
+
+
 handleCategory();
- loadVideo('02');
+ loadVideo();
