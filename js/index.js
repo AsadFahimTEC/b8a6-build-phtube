28f09a1408da74029f32d8d408d32b9da5caf1d2
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-01 23:32:35 +0600

    some functionality changed

--- js/index.js
+++ js/index.js
@@ -12,43 +12,55 @@ const handleCategory = async () => {
     `;
     tabContainer.appendChild(div);
   });
-    console.log(data.data)
+  console.log(data.data);
 };
 
-const handleLoadVideo = async(categoryId) =>{
-    // console.log(categoryId);
-    const res = await fetch(` https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-    const data = await res.json();
-    const cardContainer = document.getElementById('card-container')
-    cardContainer.innerHTML = "";
-    data.data.forEach((videos)=>{
-        console.log(videos);
-        const div = document.createElement('div');
-        div.innerHTML = `
+const handleLoadVideo = async (categoryId) => {
+  // console.log(categoryId);
+  const res = await fetch(
+    ` https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await res.json();
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  data.data.forEach((videos) => {
+    console.log(videos);
+    const div = document.createElement("div");
+    div.innerHTML = `
         <div class="card bg-gray-100 shadow-xl">
         <figure class="h-[200px]">
-            <img class="h-full" src="${videos?.thumbnail}" alt="not_available" class="w-full" />
+            <img class="h-full" src="${
+              videos?.thumbnail
+            }" alt="not_available" class="w-full" />
         </figure>
 <div class="flex justify-start gap-4 start">
             <div class="w-10 h-10 rounded-full">
-                <img src="${videos?.authors[0]?.profile_picture}" class="w-full h-full rounded-full">
+                <img src="${
+                  videos?.authors[0]?.profile_picture
+                }" class="w-full h-full rounded-full">
             </div>
         
             <div>
             <h2 class="text-base text-black font-normal">${videos.title}</h2>
             <div class="flex justify-start gap-3 my-3 text-xs">
-                <h3 class="text-gray-500 ">${videos.authors[0].profile_name}</h3>
-                <p>${videos.authors[0]?.verified? `<img class="w-4" src="./../images/verified.svg">`: ''  }</p>
+                <h3 class="text-gray-500 ">${
+                  videos.authors[0].profile_name
+                }</h3>
+                <p>${
+                  videos.authors[0]?.verified
+                    ? `<img class="w-4" src="./../images/verified.svg">`
+                    : ""
+                }</p>
             </div>
             <p class='text-sm'>${videos.others.views} views</p>
-            <p class='text-sm'>${videos.others.posted_date} timeSort</p>
+            <p class='text-sm'>${videos.others.posted_date} seconds</p>
             </div>
         </div>
       </div>
-        `
-        cardContainer.appendChild(div);
-    });
-    // console.log(data.data);
+        `;
+    cardContainer.appendChild(div);
+  });
+  // console.log(data.data);
 };
 
 handleCategory();
